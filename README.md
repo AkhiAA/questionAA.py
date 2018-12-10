62c15cde24ea100417232f5f673b085c02ac246d
csPython <cspython@AD25TF04NGF1J-17.local>
2018-12-10 10:50:41 -0500

    Accurate readMe.md

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # questionAA.py
+This program is about giving the user 4 multiple choice questions using while loops 
+and for each input the output varies
---
+++ questionsAA.py
@@ -0,0 +1,6 @@
+Python 3.5.3 (v3.5.3:1880cb95a742, Jan 16 2017, 08:49:46) 
+[GCC 4.2.1 (Apple Inc. build 5666) (dot 3)] on darwin
+Type "copyright", "credits" or "license()" for more information.
+>>> WARNING: The version of Tcl/Tk (8.5.9) in use may be unstable.
+Visit http://www.python.org/download/mac/tcltk/ for current information.
+# Question 1 
